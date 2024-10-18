98f36c92488350d38e0d799afbe4451b9184a647
Grigoriy Evdokimov <tapo4kii@gmail.com>
2024-10-18 10:30:37 +0400

    EMMA-5807 autobump patch version

--- generate.sh
+++ generate.sh
@@ -1,6 +1,16 @@
+#!/bin/bash
+
+version=$(cat version.txt)
+
+IFS='.' read -r -a version_parts <<< "$version"
+version_parts[2]=$((version_parts[2]+1))
+new_version="${version_parts[0]}.${version_parts[1]}.${version_parts[2]}"
+
+echo $new_version > version.txt
+
 openapi-generator generate \
   -i https://api.emma.ms/v3/api-docs \
   -g go \
-  --additional-properties packageName=emma,packageVersion=0.0.1,useTags=true \
+  --additional-properties packageName=emma,packageVersion="$new_version",useTags=true \
   --git-user-id emma-community \
   --git-repo-id emma-go-sdk
